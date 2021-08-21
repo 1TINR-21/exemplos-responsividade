60164e95fc6b99b3ef474323cb4f4420a5148504
Augusto Seabra <augusto.seabra00@gmail.com>
2021-08-20 22:16:14 -0300

    Adicionando algum css

--- js/script.js
+++ js/script.js
@@ -15,38 +15,12 @@ const validadeObjectParam = (object, objectKeys) => {
   }
 } 
 
-const cardGenerator = (imageSettings) => {
-  try {
-    if(!validadeObjectParam(imageSettings, { src: "", alt: "" })) throw "Parâmetro(1) imageSetting deve ser do tipo objeto no modelo { alt: '', src: '' }"
-
-    const div = document.createElement("div")
-    div.classList.add("card__item")
-  
-    const { alt, src } = imageSettings
-  
-    const img = document.createElement("img")
-    img.setAttribute("src", src)
-    img.setAttribute("alt", alt)
-  
-    div.appendChild(img)
-  
-    const cardsContainer = document.querySelector(".cards")
-    cardsContainer.appendChild(div)
-  }catch(e) {
-    console.error("Não foi possivel gerar o card.")
-    console.error(e)
-  }
-}
-
-cardGenerator({ alt: "Logo do HTML 5", src: "./imagens/html5.png" })
-cardGenerator({ alt: "Logo do CSS 3", src: "./imagens/css3.png" })
-cardGenerator({ alt: "Logo do Javascript", src: "./imagens/javascript.png" })
-
 const headerListGenerator = (linkSettings) => {
   try {
     if(!validadeObjectParam(linkSettings, { href: "", content: "" })) throw "Parâmetro(1) linkSettings deve ser do tipo objeto no modelo { href: '', content: '' }"
 
     const listItem = document.createElement("li")
+    listItem.classList.add("list__item")
     const listLink = document.createElement("a")
     const { href, content } = linkSettings
     listLink.setAttribute("href", href)
@@ -66,10 +40,48 @@ for(let i = 1; i < 4; i++) {
   headerListGenerator({ href: "#", content: `Item - ${i}` })
 }
 
+const cardGenerator = (cardSettings) => {
+  try {
+    if(!validadeObjectParam(cardSettings, { src: "", alt: "", title: "", content: "" })) throw "Parâmetro(1) cardSettings deve ser do tipo objeto no modelo { alt: '', src: '', title: '', content: '' }"
+    
+    const { alt, src, title, content } = cardSettings
+    const div = document.createElement("div")
+    div.classList.add("card__item")
+
+    const h2 = document.createElement("h2")
+    h2.classList.add("card__title")
+    h2.innerHTML = title
+    div.appendChild(h2)
+  
+    const img = document.createElement("img")
+    img.setAttribute("src", src)
+    img.setAttribute("alt", alt)
+    img.classList.add("card__image")
+    div.appendChild(img)
+
+    const paragraph = document.createElement("p")
+    paragraph.innerHTML = content
+    paragraph.classList.add("card__content")
+    div.appendChild(paragraph)
+  
+    const cardsContainer = document.querySelector(".cards")
+    cardsContainer.appendChild(div)
+  }catch(e) {
+    console.error("Não foi possivel gerar o card.")
+    console.error(e)
+  }
+}
+
+for(let i = 1; i < 15; i++) {
+  cardGenerator({ alt: `Logo ${i}`, src: "https://via.placeholder.com/400x200", title: "Lorem ipsum", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec ipsum ac ante suscipit accumsan. Vivamus id nulla eleifend tortor varius molestie. Phasellus vitae dui in arcu iaculis tincidunt. Nam eget nisl dolor. Curabitur a luctus massa. Curabitur ut lacus rutrum, accumsan ex eu, vehicula augue" })
+}
+
+
 const footerItemGenerator = (footerItemSettings) => {
   try {
     if(!validadeObjectParam(footerItemSettings, { iconPrefix: "", iconValue: "", content: "" })) throw "Parâmetro(1) footerItemSettings deve ser do tipo objeto no modelo { iconPrefix: '', iconValue: '', href: '', content: '' }, onde a chave 'href' é opcional"
     const paragraph = document.createElement("p")
+    paragraph.classList.add("footer__item")
     const icon = document.createElement("i")
     const { iconPrefix, iconValue, href, content } = footerItemSettings
     icon.classList.add(iconPrefix)
@@ -87,8 +99,6 @@ const footerItemGenerator = (footerItemSettings) => {
 
     const footerContainer = document.querySelector(".footer")
     footerContainer.appendChild(paragraph)
-
-    console.log(footerContainer)
   }catch(e) {
     console.error("Não foi possível gerar o item do rodapé.")
     console.error(e)
